1f7e74e97d01714726bb4a0dfc8068fcfaf6c312
ayush <you@example.com>
2025-06-19 21:47:49 +0530

    posted on 19/07/2025

---
+++ all.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const sections = document.querySelectorAll(".specialtext, .specialCarousel , .customer , .CERTIFICATE , .abouttext ,.abouthead ,.customerText ,.CERTIFICATEText , .footer , .MenuCarousel , .Gallery  ,.pics , .writingsection  ");
+
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.style.opacity = "1";
+        entry.target.style.transform = "translateY(0)";
+      }
+    });
+  }, { threshold: 0.1 }); // Trigger when 10% of section is visible
+
+  sections.forEach(section => observer.observe(section));
+});
+
+
+
